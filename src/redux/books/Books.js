762d50f6d95b33e125ca0dde4ceb24a79aff60a7
Mubarak <imubarak2424@gmail.com>
2022-01-21 09:05:37 +0100

    api calls

--- src/redux/books/Books.js
+++ src/redux/books/Books.js
@@ -1,5 +1,6 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const ids = 'ObbG1JsyiVwBBexxqAxs';
 
 const initialState = [];
 
@@ -8,17 +9,40 @@ export const addBook = (payload) => ({
   payload,
 });
 
+export const addBookAsync = (payload) => (dispatch) => fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${ids}/books`,
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  }).then((response) => response.text())
+  .then((data) => {
+    if (data === 'Created') { dispatch(addBook(payload)); }
+  });
+
 export const removeBook = (payload) => ({
   type: REMOVE_BOOK,
   payload,
 });
+
+export const removeBookAsync = (payload) => (dispatch) => fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${ids}/books/${payload.item_id}`, {
+  method: 'DELETE',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(payload),
+}).then((response) => response.text())
+  .then(() => {
+    dispatch(removeBook(payload.item_id));
+  });
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.item_id !== action.payload);
     default:
       return state;
   }
